24232b4114367e1cc16647a394bfd73578744a6b
sascha <sastrand@pdx.edu>
2018-11-01 10:31:50 -0700

    changed to rotation pairing approach

--- pairing.py
+++ pairing.py
@@ -1,35 +1,19 @@
 import csv
-from itertools import combinations
-
-def choose_two(class_list):
-    return list(combinations(class_list, 2))
-
-def lab_pairs(pairs, class_list):
-    print pairs
-    print len(pairs)
-    print class_list
-    accounted_for = set()
-    i = 0
-    for student in class_list:
-        while student not in accounted_for:
-            if (pairs[i][0] == student and pairs[i][1] not in accounted_for) or (pairs[i][0] not in accounted_for and pairs[i][1] == student):
-                f.write(str(pairs[i]) + "\n")
-                accounted_for.add(pairs[i][0])
-                accounted_for.add(pairs[i][1])
-                #  pairs.remove(pairs[i])
-            if i >= len(pairs)-1:
-                i = 0
-            else:
-                i = i + 1
 
 with open("./class_list.txt") as f:
     class_list = f.read().splitlines()
 
 f = open("output.txt", "w")
 
-first_drv = choose_two(class_list)
-first_nav = [p[::-1] for p in first_drv]
-class_list = class_list[::-1]
+class_list_a = class_list[:len(class_list)//2]
+class_list_b = class_list[len(class_list)//2:]
+
+def get_weeks_pairs(cl_a, cl_b):
+    cl_a.append(cl_b[-1])
+    del cl_b[-1]
+    cl_b.insert(0, cl_a[0])
+    del cl_a[0]
+    return zip(cl_a, cl_b)
 
 f.write("""
  _        ______     _        _______  ______   _______ 
@@ -42,16 +26,10 @@ f.write("""
 |/    )_)|/ \___/   (_______/|/     \||/ \___/ \_______)
                                                         \n""")
 
-lab_pairs(first_drv, class_list)
-
-#  for week in range(2,20):
-    #  f.write("\n\n---- Week {} ----\n".format(week))
-    #  f.write("\n-- Tuesday --\n")
-    #  f.write("'DRIVER','NAVIGATOR'\n")
-    #  lab_pairs(first_drv, class_list)
-    #  f.write("\n-- Thursday --\n")
-    #  f.write("'DRIVER','NAVIGATOR'\n")
-    #  lab_pairs(first_nav, class_list)
+for week in range(3,20):
+    f.write("\n\n---- Rotation {} ----\n\n".format(week))
+    for p in get_weeks_pairs(class_list_a, class_list_b):
+        f.write(str(p) + "\n")
 
 f.close()
 
